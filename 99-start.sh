7f138f0d3d2cb713bc920084bb349963738781fe
hongwei666 <ghwmail@163.com>
2017-05-23 13:50:44 +0800

    first commit

--- 99-start.sh
+++ 99-start.sh
@@ -3,6 +3,6 @@
 
 if [ "$ACTION" = ifup ]; then 
     if [ "${INTERFACE}" = "wan" ]; then
-        sleep 3 && date -u "`curl -sI 117.37.36.169:10000 | awk '/Date:/{gsub(/Jan/,"01",$4);gsub(/Feb/,"02",$4);gsub(/Mar/,"03",$4);gsub(/Apr/,"04",$4);gsub(/May/,"05",$4);gsub(/Jun/,"06",$4);gsub(/Jul/,"07",$4);gsub(/Aug/,"08",$4);gsub(/Sep/,"09",$4);gsub(/Oct/,"10",$4);gsub(/Nov/,"11",$4);gsub(/Dec/,"11",$4);print $5"-"$4"-"$3" "$6;}'`"
+        sleep 3 && /bin/start.sh
     fi
 fi
